9b33353c1bf6519cebad6c0c505b339b2f9039c1
Paul Speed <pspeed42@gmail.com>
2024-11-18 17:52:13 -0500

    Fixed an issue where 0-height objects were making shadows the clipped through everything.

--- src/main/java/com/simsilica/fx/shadow/DropShadowFilter.java
+++ src/main/java/com/simsilica/fx/shadow/DropShadowFilter.java
@@ -1,40 +1,45 @@
 /*
  * $Id$
- * 
+ *
  * Copyright (c) 2016, Simsilica, LLC
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions 
+ * modification, are permitted provided that the following conditions
  * are met:
- * 
- * 1. Redistributions of source code must retain the above copyright 
+ *
+ * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright 
- *    notice, this list of conditions and the following disclaimer in 
- *    the documentation and/or other materials provided with the 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
  *    distribution.
- * 
- * 3. Neither the name of the copyright holder nor the names of its 
- *    contributors may be used to endorse or promote products derived 
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package com.simsilica.fx.shadow;
+
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+import org.slf4j.*;
 
 import com.jme3.asset.AssetManager;
 import com.jme3.bounding.BoundingBox;
@@ -63,8 +68,6 @@ import com.jme3.shadow.ShadowUtil;
 import com.jme3.texture.FrameBuffer;
 import com.jme3.texture.Texture;
 import com.jme3.util.BufferUtils;
-import java.nio.FloatBuffer;
-import java.nio.ShortBuffer;
 
 
 /**
@@ -74,6 +77,8 @@ import java.nio.ShortBuffer;
  *  @author    Paul Speed
  */
 public class DropShadowFilter extends Filter {
+
+    private static Logger log = LoggerFactory.getLogger(DropShadowFilter.class);
 
     private static final int VERTS_PER_SHADOW = 8; // one per box corner
     private static final int TRIS_PER_SHADOW = 12; // two per face
@@ -109,7 +114,7 @@ public class DropShadowFilter extends Filter {
     private Material shadowMaterial;
     private Mesh mesh;
     private int maxShadows;
-    
+
     private ColorRGBA shadowColor = new ColorRGBA(0, 0, 0, 0.75f);
 
     private VertexBuffer vbPos;
@@ -124,7 +129,7 @@ public class DropShadowFilter extends Filter {
     private boolean showBox = false;
 
     /**
-     *  Creates a drop shadow filter that will show shadows, up to the 500 nearest 
+     *  Creates a drop shadow filter that will show shadows, up to the 500 nearest
      *  shadow-casting objects.
      */
     public DropShadowFilter() {
@@ -142,11 +147,11 @@ public class DropShadowFilter extends Filter {
     public void setShadowIntensity( float f ) {
         shadowColor.a = f;
     }
-    
+
     public float getShadowIntensity() {
         return shadowColor.a;
     }
-    
+
     /**
      *  Turns on a debug view where the full box is rendered
      *  using the texture coordinates as colors.
@@ -157,10 +162,10 @@ public class DropShadowFilter extends Filter {
             shadowMaterial.setBoolean("ShowBox", showBox);
         }
     }
-    
+
     public boolean getShowBox() {
         return showBox;
-    }    
+    }
 
     @Override
     protected boolean isRequiresDepthTexture() {
@@ -169,7 +174,7 @@ public class DropShadowFilter extends Filter {
 
     @Override
     protected void initFilter( AssetManager assets, RenderManager rm, ViewPort vp, int w, int h ) {
-    
+
         // Cheating... side effect of being lazy and using a filter
         // without actually needing to filter anything.
         material = new Material(assets, "MatDefs/shadow/Null.j3md");
@@ -195,7 +200,7 @@ public class DropShadowFilter extends Filter {
         m.setColor("ShadowColor", shadowColor);
         m.setBoolean("ShowBox", showBox);
         m.getAdditionalRenderState().setDepthWrite(false);
-        m.getAdditionalRenderState().setDepthTest(false); 
+        m.getAdditionalRenderState().setDepthTest(false);
         m.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
         shadowGeom.setMaterial(m);
         shadowGeom.setLocalTranslation(0, 100, 0);
@@ -214,7 +219,7 @@ public class DropShadowFilter extends Filter {
 
     @Override
     protected void postFrame( RenderManager renderManager, ViewPort viewPort, FrameBuffer prevFilterBuffer, FrameBuffer sceneBuffer ) {
- 
+
         RenderQueue rq = viewPort.getQueue();
         for (Spatial scene : viewPort.getScenes()) {
             ShadowUtil.getGeometriesInCamFrustum(scene, viewPort.getCamera(), ShadowMode.Cast, casters);
@@ -226,15 +231,15 @@ public class DropShadowFilter extends Filter {
         BoundingSphere cullCheck = new BoundingSphere();
         Vector3f pos = new Vector3f();
 
-        Texture frameTex = prevFilterBuffer.getColorBuffer().getTexture(); 
-        Texture depthTex = prevFilterBuffer.getDepthBuffer().getTexture(); 
+        Texture frameTex = prevFilterBuffer.getColorBuffer().getTexture();
+        Texture depthTex = prevFilterBuffer.getDepthBuffer().getTexture();
         shadowMaterial.setTexture("FrameTexture", frameTex);
         if( frameTex.getImage().getMultiSamples() > 1 ) {
             shadowMaterial.setInt("NumSamples", frameTex.getImage().getMultiSamples());
         } else {
             shadowMaterial.clearParam("NumSamples");
         }
-        
+
         shadowMaterial.setTexture("DepthTexture", depthTex);
         if( depthTex.getImage().getMultiSamples() > 1 ) {
             shadowMaterial.setInt("NumSamplesDepth", depthTex.getImage().getMultiSamples());
@@ -274,12 +279,39 @@ public class DropShadowFilter extends Filter {
             // in many cases and will produce better shadows for oblong
             // objects than a simple round radius would.
             BoundingBox bounds = (BoundingBox)g.getModelBound();
-            
+
             float scale = g.getWorldScale().x;
             float xEx = bounds.getXExtent() * scale;
             float yEx = bounds.getYExtent() * scale;
             float zEx = bounds.getZExtent() * scale;
             //float volumeHeight = Math.max(yEx, Math.min(xEx,zEx));
+
+            // 2024-11-17 fixing a bug where fully flat geometry generates shadows
+            // that clip through everything.
+            //if( yEx == 0 ) {
+            //    // The object has no vertical size and will end up creating
+            //    // a very strange shadow if we don't adjust the y size slightly
+            //    yEx = 0.1f;
+            //}
+            // But instead of that, I'm resurrexting the volumeHeight code.  I no
+            // longer remember why I didn't use it but it seems appropriate to base
+            // the height of the cast shadow on the horizontal sizes... else a paper
+            // thin large box wouldn't cast a shadow at all.  If I find a problem
+            // with this then I should document it... and maybe find a different
+            // way to fix that new issue.
+            //yEx = Math.max(yEx, Math.min(xEx,zEx));
+            // The issue is that for big things that are very flat we cannot put the
+            // shadow bubble close enough to the object and so when the big flat object
+            // is close to the ground, it essentially has no shadow until you lift it up.
+            // For very flat objects, it's also going to be very dramatic.
+            // And actually, the way the offset is calculated, it's worse than that because
+            // the shadow box ends up way below the object, not even intersecting it.
+            // If we swap out the yEx for something else then we need to offset the
+            // shadow box so that it still clips the object pretty well.  And for very thin
+            // objects we'd end up casting shadow above the object, too.  Need to change
+            // the shape of the egg to be squatier up top and offset appropriately, etc..
+            // In the mean time, we will at least give it a minimum value.
+            yEx = Math.max(0.1f, yEx);
 
             //float xOffset = bounds.getCenter().x * scale;
             //float yOffset = bounds.getCenter().y * scale;
@@ -298,38 +330,38 @@ public class DropShadowFilter extends Filter {
             float volumeHeight;
             float radius;
             float cubeEpsilon = 0.01f;
-            if( Math.abs(xEx - yEx) < cubeEpsilon 
-                && Math.abs(xEx - zEx) < cubeEpsilon 
+            if( Math.abs(xEx - yEx) < cubeEpsilon
+                && Math.abs(xEx - zEx) < cubeEpsilon
                 && Math.abs(yEx - zEx) < cubeEpsilon ) {
                 // Then the object is basically cubical and it doesn't
                 // make any sense to treat it as anything other than a
                 // "sphere" for purposes of the shadow box.
                 volumeHeight = xEx; // doesn't matter as they are all the same
                 radius = xEx; // doesn't matter as they are all the same
-            } else {            
+            } else {
                 // Then the object is oblong in some way so we'll try to
                 // approximate it's shape projected into the x/z plane.
-                 
+
                 // Let's try calculating better box dimensions by projecting
                 // extent axis rays into yaw space.  Hopefully an approximation
                 // better than a sphere while still adapting well to random non-yaw
                 // orientations.
                 // "yaw space" is our quaternion without the yaw component...
-                // we'll be rotating the shadow box by the yaw component later. 
+                // we'll be rotating the shadow box by the yaw component later.
                 Quaternion yawSpace = new Quaternion().fromAngles(angles[0], 0, angles[2]);
                 Vector3f xAxis = yawSpace.mult(new Vector3f(xEx, 0, 0));
                 Vector3f yAxis = yawSpace.mult(new Vector3f(0, yEx, 0));
                 Vector3f zAxis = yawSpace.mult(new Vector3f(0, 0, zEx));
-            
+
                 xEx = Math.max(Math.max(Math.abs(xAxis.x), Math.abs(yAxis.x)), Math.abs(zAxis.x));
                 yEx = Math.max(Math.max(Math.abs(xAxis.y), Math.abs(yAxis.y)), Math.abs(zAxis.y));
                 zEx = Math.max(Math.max(Math.abs(xAxis.z), Math.abs(yAxis.z)), Math.abs(zAxis.z));
-                volumeHeight = yEx; 
-                
-                // Rought worst-case radius for frustum checking               
+                volumeHeight = yEx;
+
+                // Rought worst-case radius for frustum checking
                 radius = Math.max(xEx, Math.max(yEx, zEx));
-            } 
-            
+            }
+
             BoundingBox worldBounds = (BoundingBox)g.getWorldBound();
             pos.set(worldBounds.getCenter());
             pos.y -= yEx;
@@ -351,7 +383,6 @@ public class DropShadowFilter extends Filter {
             }
 
             boxScale.set(0.5f/xEx, 0.5f/volumeHeight, 0.5f/zEx);
-
 
             Quaternion rotation = new Quaternion().fromAngles(0, angles[1], 0);
             Quaternion invRotation = rotation.inverse();
@@ -421,12 +452,12 @@ public class DropShadowFilter extends Filter {
             shadowGeom.updateGeometricState();
             renderManager.renderGeometry(shadowGeom);
         }
-        
+
         casters.clear();
     }
 
     private class CasterComparator implements GeometryComparator {
-    
+
         private Camera cam;
         private final Vector3f tempVec  = new Vector3f();
         private final Vector3f tempVec2 = new Vector3f();
